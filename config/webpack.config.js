636a6a422a589c76b0a8184a56967552d028fd91
Tushar <jtushar53@gmail.com>
2018-08-01 12:48:26 +0530

    added workaround from AlexBDev

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -2,9 +2,9 @@ var path = require('path');
 var useDefaultConfig = require('@ionic/app-scripts/config/webpack.config.js');
 
 module.exports = function () {
-  console.log(process.env.IONIC_ENV);
+  console.log(process.env.APP_ENV);
     useDefaultConfig[process.env.IONIC_ENV].resolve.alias = {
-        "@environment": path.resolve(__dirname + '/../src/config/config.' + process.env.IONIC_ENV + '.ts'),
+        "@environment": path.resolve(__dirname + '/../src/config/config.' + process.env.APP_ENV + '.ts'),
     };
     return useDefaultConfig;
 };
